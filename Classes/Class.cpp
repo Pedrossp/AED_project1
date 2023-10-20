b453072dd907c592df9e46278508ddd86bbf1b8f
AnaSofiaBaptista <anasofiab.27@gmail.com>
2023-10-20 21:05:16 +0100

    Class with functions

--- Classes/Class.cpp
+++ Classes/Class.cpp
@@ -1,1 +1,34 @@
 #include "Class.h"
+
+Class::Class(string& ClassCode, string& UcCode, string& Weekday, float& StartHour, float& Duration, string& Type) {
+    ClassCode_ = ClassCode;
+    UcCode_ = UcCode;
+    Weekday_ = Weekday;
+    StartHour_ = StartHour;
+    Duration_ = Duration;
+    Type_ = Type;
+}
+
+string Class::get_classcode() {
+    return ClassCode_;
+}
+
+string Class::get_uccode() {
+    return UcCode_;
+}
+
+string Class::get_weekday() {
+    return Weekday_;
+}
+
+float Class::get_starthour() {
+    return StartHour_;
+}
+
+float Class::get_duration() {
+    return Duration_;
+}
+
+string Class::get_type() {
+    return Type_;
+}
--- Classes/Class.h
+++ Classes/Class.h
@@ -1,8 +1,30 @@
 #ifndef AED_PROJECT1_CLASS_H
 #define AED_PROJECT1_CLASS_H
 
+#include <string>
+
+using namespace std;
+
 
 class Class {
+public:
+    Class(string& ClassCode, string& UcCode, string& Weekday, float& StartHour, float& Duration, string& Type);
+    string get_classcode();
+    string get_uccode();
+    string get_weekday();
+    float get_starthour();
+    float get_duration();
+    string get_type();
+
+
+private:
+    string ClassCode_;
+    string UcCode_;
+    string Weekday_;
+    float StartHour_;
+    float Duration_;
+    string Type_;
+
 
 };
 
