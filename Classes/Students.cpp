cdf94d763de3a2d888fddb82190feb17d2e777e3
Eduardo Santos <edu.santos04.vc@gmail.com>
2023-10-20 11:04:35 +0100

    Classes folder

---
+++ Classes/Students.cpp
@@ -0,0 +1,5 @@
+//
+// Created by edu on 20-10-2023.
+//
+
+#include "Students.h"
---
+++ Classes/Students.h
@@ -0,0 +1,14 @@
+//
+// Created by edu on 20-10-2023.
+//
+
+#ifndef AED_PROJECT_STUDENTS_H
+#define AED_PROJECT_STUDENTS_H
+
+
+class Students {
+
+};
+
+
+#endif //AED_PROJECT_STUDENTS_H
