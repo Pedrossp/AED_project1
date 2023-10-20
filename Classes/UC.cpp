4890cc7307f7ad88f25689a354651691244c6f49
PP <pedrosspedro@sapo.pt>
2023-10-20 20:40:24 +0100

    UC class update

--- Classes/UC.cpp
+++ Classes/UC.cpp
@@ -1,1 +1,9 @@
 #include "UC.h"
+
+UC::UC(string UcCode){
+    UcCode_ = UcCode;
+}
+
+string UC::get_code() {
+    return UcCode_;
+}
--- Classes/UC.h
+++ Classes/UC.h
@@ -1,8 +1,17 @@
 #ifndef AED_PROJECT1_UC_H
 #define AED_PROJECT1_UC_H
+#include <string>
+
+using namespace std;
 
 
 class UC {
+private:
+    string UcCode_;
+
+public:
+    UC(string UcCode);
+    string get_code();
 
 };
 
