5044e084d172cadf40b935cd83ace4909f110047
PP <pedrosspedro@sapo.pt>
2023-10-20 19:39:16 +0100

    teste

--- Classes/Students.h
+++ Classes/Students.h
@@ -14,8 +14,8 @@ private:
 
 
 public:
-    Students(string &StudentName, int StudentCode, vector & <pair<UC, Class>> UC_Class);
-    string &get_name();
+    Students(string &StudentName, int StudentCode, vector& <pair<UC, Class>> UC_Class);
+    string& get_name();
     int get_code();
 
 };
