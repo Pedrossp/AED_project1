05383a9d893be65b4629ce6894b690f60bb2599c
Eduardo Santos <edu.santos04.vc@gmail.com>
2023-10-20 19:29:09 +0100

    StudentCode changed to string

--- Classes/Students.h
+++ Classes/Students.h
@@ -9,14 +9,14 @@ using namespace std;
 class Students {
 private:
     string StudentName;
-    int StudentCode;
+    string StudentCode;
     vector<pair<UC, Class>> UC_Class;
 
 
 public:
     Students(string &StudentName, int StudentCode, vector & <pair<UC, Class>> UC_Class);
-    string &get_name();
-    int get_code();
+    string&get_name();
+    string& get_code();
 };
 
 #endif //AED_PROJECT_STUDENTS_H
