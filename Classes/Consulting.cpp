4dafcde088ec79309cc74313663dd1e952b83dbd
Eduardo Santos <edu.santos04.vc@gmail.com>
2023-10-31 17:15:59 +0000

    creating Consulting

---
+++ Classes/Consulting.cpp
@@ -0,0 +1,5 @@
+//
+// Created by edu on 31-10-2023.
+//
+
+#include "Consulting.h"
---
+++ Classes/Consulting.h
@@ -0,0 +1,16 @@
+//
+// Created by edu on 31-10-2023.
+//
+
+#ifndef AED_PROJECT1_CONSULTING_H
+#define AED_PROJECT1_CONSULTING_H
+
+
+class Consulting {
+public:
+    list<Student*> filterYear(char number ,)
+
+};
+
+
+#endif //AED_PROJECT1_CONSULTING_H
